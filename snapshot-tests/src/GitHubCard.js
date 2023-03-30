87b6c75f3e0e98de2bfc4138efe19a8b09eb237b
leslieportiz <lorraineportiz@gmail.com>
2023-03-29 17:08:29 -0700

    snapshot testing completed

---
+++ snapshot-tests/src/GitHubCard.js
@@ -0,0 +1,9 @@
+import { Card } from "react-bootstrap";
+
+export default function GitHubCard() {
+    return <Card>
+        <h1>INS140</h1>
+        <img src='photo.webp' alt='private photo'/>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce porttitor id magna nec maximus. Fusce pharetra imperdiet egestas. Maecenas finibus ultricies rutrum. Proin non tellus vel risus molestie lobortis. Donec tristique massa in velit pharetra consequat. Aliquam non accumsan purus. Nullam faucibus turpis egestas lectus accumsan imperdiet. Donec hendrerit consectetur velit quis finibus. Sed quis nibh lectus. Morbi nec lobortis diam. Nulla nec ante felis. Duis bibendum urna sapien. Proin et enim non turpis eleifend tincidunt in in velit.</p>
+    </Card>
+}
